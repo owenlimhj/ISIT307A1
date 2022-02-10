f92bf52698513d1733ba77660027aed2244fde00
owenlimhj <93114755+owenlimhj@users.noreply.github.com>
2022-02-10 15:19:52 +0800

    Update Subject_Page.php

--- A2/Subject_Page.php
+++ A2/Subject_Page.php
@@ -18,7 +18,7 @@ if (isset($_POST["studentnum"]))
 ?>
     <h2>Choose your test subject</h2><br>
     <button class="subjectbtn" onclick="window.location.href = 'History_Page.php';" >History</button><br><br>
-    <button class="subjectbtn" onclick="window.location.href = 'Math_Page.php';" >Math</button><br><br>
+    <button class="subjectbtn" onclick="window.location.href = 'Math_Page.php';" >Mathematics</button><br><br>
     <button class="exit" onclick="window.location.href = 'Exit_Page.php';" style="display: block;">Exit</button>
 </div>
 </body>
